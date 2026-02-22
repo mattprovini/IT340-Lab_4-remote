a5f4683fa32dca68d4966eb9a4a4dec647d4dc39
Matthew Provini <provinimatthew@gmail.com>
2026-02-22 17:15:13 -0500

    Part 3 Step 7

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Log the date and memory usage
 
-echo "Memory Log - $(date)" >> system_log.txt
-free -h | grep Mem >> system_log.txt
-echo "--------------------------------" >> system_log.txt
+LOGFILE="/home/matthew/Desktop/Lab_4/system_log.txt"
+
+echo "DAILY MEMORY CHECK - $(date)" >> "$LOGFILE"
+free -h | grep Mem >> "$LOGFILE"
+echo "--------------------------------" >> "$LOGFILE"
